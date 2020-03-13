8009d04649af84ceb794a363d811d28144d60c12
rivaldipalermo <rivaldi.10518166@mahasiswa.unikom.ac.id>
2020-03-13 16:00:58 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function education(){
+    let content = document.querySelector("#content");
+    content.innerHTML = "<h1>Education</h1><section class='contents' id='contents'><ul class='spacing'><li>SD Negeri Pangipasan</li><li>SMP Negeri 1 Soreang</li><li>SMK Negeri 1 Katapang</li><li>Universitas Komputer Indonesia</li></ul></section>";
+}
+
+function experience(){
+    let content = document.querySelector("#content");
+    content.innerHTML = "<h1>Experience</h1><section class='contents' id='contents'><p>Junior Web Developer in Biru Tekno</p><p>Wordpress Developer in PT Multi Taruna Sejati</p></section>";
+}
+
+function contact(){
+    let content = document.querySelector("#content");
+    content.innerHTML = "<h1>Contact</h1><section class='contents' id='contents'><ul class='spacing'><li>Facebook : Rivaldi Palermo</li><li>Phone : +6289638434942</li><ul></section>";
+}
